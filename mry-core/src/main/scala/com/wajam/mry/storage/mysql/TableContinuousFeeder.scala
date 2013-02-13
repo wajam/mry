aae7c89c5ff4375dd09dfadb0018dd333dfdaec7
Olivier Toupin <olivier.toupin@wajam.com>
2013-02-13 13:43:55 -0500

    Reverting code cleanup unrelated to my changes

--- mry-core/src/main/scala/com/wajam/mry/storage/mysql/TableContinuousFeeder.scala
+++ mry-core/src/main/scala/com/wajam/mry/storage/mysql/TableContinuousFeeder.scala
@@ -29,7 +29,7 @@ class TableContinuousFeeder(name: String ,storage: MysqlStorage, table: Table, t
       try {
         val record = new Record()
         record.token = data(Token).toString.toLong
-        record.timestamp = NrvTimestamp(data(Timestamp).asInstanceOf[NrvTimestamp])
+        record.timestamp = com.wajam.nrv.utils.timestamp.Timestamp(data(Timestamp).toString.toLong)
         val keys = data(Keys).asInstanceOf[Seq[String]]
         record.accessPath = new AccessPath(keys.map(new AccessKey(_)))
         Some(record)
