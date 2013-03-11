9109644c8ff3a20a67ce634e02474d24ca504773
Alexandre Bergeron <alex@waj.am>
2013-03-11 13:10:14 -0400

    Removed useless vars

--- mry-core/src/main/scala/com/wajam/mry/execution/Variable.scala
+++ mry-core/src/main/scala/com/wajam/mry/execution/Variable.scala
@@ -4,7 +4,7 @@ package com.wajam.mry.execution
  * Variable that can points to nothing or to a value
  */
 @SerialVersionUID(-2511774283683367275L)
-class Variable(var block: Block, var id: Int, var value: Value = NullValue) extends Object with OperationSource with OperationApi with Serializable {
+class Variable(val block: Block, val id: Int, var value: Value = NullValue) extends Object with OperationSource with OperationApi with Serializable {
   def sourceBlock = block
 
   override def proxiedSource = Some(value)
