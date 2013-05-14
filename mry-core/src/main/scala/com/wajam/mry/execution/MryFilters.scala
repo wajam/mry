a68f1af2c3c2938ad9047636dae15ab19ccfb7e0
Olivier Toupin <olivier.toupin@wajam.com>
2013-05-14 11:07:47 -0400

    MryFilter implementations as case classes

--- mry-core/src/main/scala/com/wajam/mry/execution/MryFilters.scala
+++ mry-core/src/main/scala/com/wajam/mry/execution/MryFilters.scala
@@ -8,49 +8,68 @@ object MryFilters extends Enumeration {
   val GreaterThanOrEqual = Value(3, "gte")
   val LesserThan = Value(4, "lt")
   val LesserThanOrEqual = Value(5, "lte")
-  
-  private def throwMatchError(left: com.wajam.mry.execution.Value,
-                              right: com.wajam.mry.execution.Value): Boolean = {
-    throw new RuntimeException("Unsupported type for filter: left:%s, right:%s".format(left.getClass, right.getClass))  
+
+  def applyFilter(left: Object, filter: MryFilters.MryFilter, right: Object): Boolean = {
+    val operator = filter match {
+      case Equals => EqualsFilter()
+      case GreaterThan => GreaterThanFilter()
+      case GreaterThanOrEqual => GreaterThanOrEqualFilter()
+      case LesserThan => LesserThanFilter()
+      case LesserThanOrEqual => LesserThanOrEqualFilter()
+    }
+
+    operator.execute(left, right)
   }
 
-  private val operatorsFunctions: Map[MryFilter, (Object, Object) => Boolean] = Map(
-    Equals -> ((left: Object, right: Object) => {
-      left.value.equalsValue(right.value)
-    }),
+  trait Filter {
+    def execute(left: Object, right: Object): Boolean
 
-    GreaterThan -> ((left: Object, right: Object) => {
+    protected def throwMatchError(left: com.wajam.mry.execution.Value,
+                                right: com.wajam.mry.execution.Value): Boolean = {
+      throw new RuntimeException("Unsupported type for filter: left:%s, right:%s".format(left.getClass, right.getClass))
+    }
+  }
+
+  case class EqualsFilter() extends Filter {
+    def execute(left: Object, right: Object) = {
+      left.value.equalsValue(right.value)
+    }
+  }
+
+  case class GreaterThanFilter() extends Filter {
+    def execute(left: Object, right: Object) = {
       (left.value, right.value) match {
         case (l: IntValue, r: IntValue) => l.intValue > r.intValue
         case (l, r) => throwMatchError(l, r)
       }
-    }),
+    }
+  }
 
-    GreaterThanOrEqual -> ((left: Object, right: Object) => {
+  case class GreaterThanOrEqualFilter() extends Filter {
+    def execute(left: Object, right: Object) = {
       (left.value, right.value) match {
         case (l: IntValue, r: IntValue) => l.intValue >= r.intValue
         case (l, r) => throwMatchError(l, r)
       }
-    }),
+    }
+  }
 
-    LesserThan -> ((left: Object, right: Object) => {
+  case class LesserThanFilter() extends Filter {
+    def execute(left: Object, right: Object) = {
       (left.value, right.value) match {
         case (l: IntValue, r: IntValue) => l.intValue < r.intValue
         case (l, r) => throwMatchError(l, r)
       }
-    }),
+    }
+  }
 
-    LesserThanOrEqual -> ((left: Object, right: Object) => {
+  case class LesserThanOrEqualFilter() extends Filter {
+    def execute(left: Object, right: Object) = {
       (left.value, right.value) match {
         case (l: IntValue, r: IntValue) => l.intValue <= r.intValue
         case (l, r) => throwMatchError(l, r)
       }
-    })
-  )
-
-  def applyFilter(left: Object, filter: MryFilters.MryFilter, right: Object): Boolean = {
-    val operator = operatorsFunctions(filter)
-
-    operator(left, right)
+    }
   }
+
 }
