1a6cd6a9c63cf83ee17c5cc96b57e492a39ed4d4
Olivier Toupin <olivier.toupin@wajam.com>
2013-10-07 17:54:40 -0400

    * V2

--- mry-core/src/test/scala/com/wajam/mry/api/protobuf/BenchmarkRunner.scala
+++ mry-core/src/test/scala/com/wajam/mry/api/protobuf/BenchmarkRunner.scala
@@ -1,53 +1,99 @@
 package com.wajam.mry.api.protobuf
 
+import com.wajam.mry.execution.Transaction
+import com.wajam.mry.execution.Implicits._
+
+
 object BenchmarkRunner extends App {
 
   val repetition = Seq(10, 100, 1000, 2500, 5000, 10000, 50000, 100000)
 
-  def warmUp(name: String, repeat: Int, method: (Int) => Unit) = {
-    method(repeat)
+  private def randomTransaction(opCount: Int) = {
+    val t = new Transaction
+    val v = t.from("followees").get(Long.MaxValue)
+
+    for (i <- 1 to opCount)
+      v.from("followeers").delete(Long.MaxValue)
+
+    t
   }
 
-  def runTest(name: String, repeat: Int, value: Int, method: (Int) => Unit, sizef: () => Int) = {
-
-    System.out.println(s"Testing $name with value: $value...")
+  def timed(method: () => Unit): Long = {
 
     val startTime = System.currentTimeMillis()
 
-    method(repeat)
+    method()
 
     val endTime = System.currentTimeMillis()
 
-    val ms = endTime - startTime
-    val size = sizef()
-    val ratio = value * 1.0/ms
+    endTime - startTime
+  }
 
-    System.out.println(s"Took ${ms}ms, size was: ${size/1024}KB.")
-    System.out.println(f"t/m ratio: ${ms*1.0/value}%2.2f.")
+  def warmUp(warmRepeat: Int, warmCount: Int) {
+      System.out.println(s"Warming up...")
+
+      for(i <- 1 to warmRepeat) {
+        val trx = randomTransaction(warmCount)
+        val t = new ProtobufTranslator()
+
+        val bytes = t.encodeTransaction(trx)
+        t.decodeTransaction(bytes)
+      }
+  }
+
+
+  def runTest(name: String, value: Int, fct: () => Unit, size: () => Int) = {
+    System.out.println(s"Testing $name with value: $value...")
+
+    val runMs = timed (fct)
+
+    System.out.println(s"Run took ${runMs}ms, size was: ${size()/1024}KB.")
+    System.out.println(f"t/n ratio: ${runMs*1.0/value}%2.2f.")
+  }
+
+  def runTests(value: Int) = {
+
+    System.out.println(s"--Encode/Decode pair with $value--")
+
+    val t = new ProtobufTranslator()
+
+    System.out.println(s"Setting up for: $value...")
+
+    var trx: Transaction = null
+    val setupMs = timed ( () => {
+        trx = randomTransaction(value)
+      }
+    )
+
+    System.out.println(s"Setup took ${setupMs}ms.")
+
+    var bytes: Array[Byte] = null
+
+    runTest(
+      "Encoding",
+      value,
+      () =>  { bytes = t.encodeTransaction(trx) },
+      () => {bytes.length}
+    )
+
+    runTest(
+      "Decoding",
+      value,
+      () =>  { t.decodeTransaction(bytes) },
+      () => {bytes.length}
+    )
+
+    System.out.println()
   }
 
   // Warmup
-  val warmRepeat = 10000
-
-  {
-    System.out.println(s"Warming up...")
-    val b = new ProtobufTranslatorBenchmark(repetition(0))
-    b.setUp()
-    warmUp("Encoding", warmRepeat, b.timeProtobufEncode _)
-    warmUp("Decoding", warmRepeat, b.timeProtobufDecode _)
-    System.out.println()
-  }
+  warmUp(10000, 10)
 
   // Real test
   val repeat = 1
 
-  repetition.foreach { r =>
-    System.out.println(s"--Encode/Decode pair with $r--")
-    val b = new ProtobufTranslatorBenchmark(r)
-    b.setUp()
-    runTest("Encoding", repeat, r, b.timeProtobufEncode _, b.encodingSize _)
-    runTest("Decoding", repeat, r, b.timeProtobufDecode _, b.encodingSize _)
-    System.out.println()
+  repetition.foreach { value =>
+    runTests(value)
   }
 
 }
